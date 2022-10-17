16ced809b8e80e0da5ba13f67f49b6d23066b910
pc <rahimjangi1982@gmail.com>
2022-10-17 19:32:32 +0200

    first commit

---
+++ RatingAPI/src/test/java/com/raiseup/RatingAPI/RatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiseup.RatingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
